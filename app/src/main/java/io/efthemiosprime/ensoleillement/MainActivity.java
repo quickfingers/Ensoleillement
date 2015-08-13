7d2730981831020c85f96cf4b7af6739794fd33e
bong <bong@Catherines-MacBook-Air.local>
2015-08-12 23:43:46 -0400

    adding the refresh button for debugging

--- app/src/main/java/io/efthemiosprime/ensoleillement/MainActivity.java
+++ app/src/main/java/io/efthemiosprime/ensoleillement/MainActivity.java
@@ -1,5 +1,7 @@
 package io.efthemiosprime.ensoleillement;
 
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
